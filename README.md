bb07e8fc33d60f89bc88b8a28e1b0ff9f2620a40
lf-fernandes <luiz_fernandofm@hotmail.com>
2022-06-09 15:26:01 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-ybp4vb
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-ybp4vb)
---
+++ aulastaart.js
@@ -0,0 +1,8 @@
+const bandeiras = [
+['Brasil', ['Verde', 'amarelo', 'azul', 'branco']],
+['canada', ['vermelho']],
+['méxico', ['vermelho', 'verde','branco']],
+['EUA', ['azul', 'branco', 'vermelho']],
+];
+
+console.log(bandeiras[2][1][1]);
